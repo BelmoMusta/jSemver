ba825b2bfc5d39139d4afb03be1cbbed83c73be3
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2023-11-04 18:51:11 +0100

    add comparing functions (#1)
    * add comparing functions
    
    * add comparing functions as strings
    
    * add comparing functions as strings

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,9 +0,0 @@
-
-<
-build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
-^
-.src/main/java/org/mustabelmo/jsemver/Main.java,e/6/e636d49b12f18765a438a5ba7e1d2bf41a469cb2
-`
-0src/main/java/org/mustabelmo/jsemver/Semver.java,2/f/2f944998d979581d7550d8b19dfb903c8d684007
-d
-4src/test/java/org/mustabelmo/jsemver/SemverTest.java,7/4/749db62f2bad5f59bab5e1e7ba8575985d9dc975
--- src/test/java/org/mustabelmo/jsemver/SemverTest.java
+++ src/test/java/org/mustabelmo/jsemver/SemverTest.java
@@ -3,6 +3,8 @@ package org.mustabelmo.jsemver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 public class SemverTest {
     @Test
     public void testInitSemver() {
@@ -24,9 +26,9 @@ public class SemverTest {
     @Test
     public void testFunctions() {
         Semver semver = Semver.parse("1.3.5");
-        Assertions.assertEquals(1,semver.major());
-        Assertions.assertEquals(3,semver.minor());
-        Assertions.assertEquals(5,semver.patch());
+        Assertions.assertEquals(1, semver.major());
+        Assertions.assertEquals(3, semver.minor());
+        Assertions.assertEquals(5, semver.patch());
     }
 
     @Test
@@ -58,5 +60,131 @@ public class SemverTest {
     public void testSemverCoerceIncorrectVersion() {
         Semver semver = Semver.coerce("v");
         Assertions.assertFalse(semver.isValid());
+    }
+
+    @Test
+    public void testSemverCompareToNull() {
+        Semver semverA = Semver.parse("1.0.0");
+        int compare = semverA.compareTo(null);
+        Assertions.assertEquals(1, compare);
+    }
+    @Test
+    public void testSemverInvalidCompareToNull() {
+        Semver semverA = Semver.parse("xxx");
+        Semver semverB = Semver.parse("1.0.0");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(-1, compare);
+    }
+
+    @Test
+    public void testSemverCompareToInvalid() {
+        Semver semverA = Semver.parse("1.0.0");
+        Semver semverB = Semver.parse("xxx");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(1, compare);
+    }
+    @Test
+    public void testSemverCompareTo() {
+        Semver semverA = Semver.parse("1.0.0");
+        Semver semverB = Semver.parse("1.0.1");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(-1, compare);
+    }
+    @Test
+    public void testSemverCompareTo2() {
+        Semver semverA = Semver.parse("1.2.3");
+        Semver semverB = Semver.parse("1.2.3");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(0, compare);
+    }
+    @Test
+    public void testSemverCompareTo3() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("1.2.300000000");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(1, compare);
+    }
+    @Test
+    public void testSemverCompareToEquality() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("10.2.3");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertEquals(0, compare);
+    }
+    @Test
+    public void testSemverEquqls() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("10.2.3");
+        int compare = semverA.compareTo(semverB);
+        Assertions.assertTrue(semverA.equals(semverB));
+    }
+    @Test
+    public void testSemverEquqlsNull() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertFalse(semverA.equals(null));
+    }
+    @Test
+    public void testSemverHashCode() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertEquals(Objects.hash(semverA.major(),
+                semverA.minor(),
+                semverA.patch()), semverA.hashCode());
+    }
+    @Test
+    public void testSemverIsGreaterThanAsAString() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isGreaterThan("10.2.0"));
+    }
+    @Test
+    public void testSemverIsGreaterThan() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("10.2.0");
+        Assertions.assertTrue(semverA.isGreaterThan(semverB));
+    }
+
+    @Test
+    public void testSemverIsGreaterThanOrEqual() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isGreaterThanOrEqual(semverB));
+    }
+
+    @Test
+    public void testSemverIsGreaterThanOrEqualAsAString() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isGreaterThanOrEqual("10.2.3"));
+    }
+
+    @Test
+    public void testSemverIsLessThanAsAString() {
+        Semver semverA = Semver.parse("1.2.3");
+        Assertions.assertTrue(semverA.isLessThan("10.2.0"));
+    }
+    @Test
+    public void testSemverIsLessThan() {
+        Semver semverA = Semver.parse("1.2.3");
+        Semver semverB = Semver.parse("10.2.0");
+        Assertions.assertTrue(semverA.isLessThan(semverB));
+    }
+    @Test
+    public void testSemverIsLessThanOrEqual() {
+        Semver semverA = Semver.parse("10.2.3");
+        Semver semverB = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isLessThanOrEqual(semverB));
+    }
+    @Test
+    public void testSemverIsLessThanOrEqualAsAString() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isLessThanOrEqual("10.2.3"));
+    }
+    @Test
+    public void testSemverIsInRange() {
+        Semver semverA = Semver.parse("10.2.3");
+        Assertions.assertTrue(semverA.isInRange("10.2.0", "10.2.4"));
+    }
+    @Test
+    public void testSemverIsNotInRange() {
+        Semver semverA = Semver.parse("1.2.3");
+        Assertions.assertFalse(semverA.isInRange("10.2.0", "10.2.4"));
     }
 }
